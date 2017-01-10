7a5e48bff75521bc294c1633144227c06bf653d4
(cdep)illabout <cdep.illabout@gmail.com>
2017-01-10 09:24:39 +0900

    Parse [Doc] from [Control] using Parsec's token parser.

--- src/Text/Heterocephalus/Parse.hs
+++ src/Text/Heterocephalus/Parse.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Text.Heterocephalus.Parse where
@@ -15,9 +16,10 @@ import Data.Char (isUpper)
 import Data.Data (Data)
 import Data.Typeable (Typeable)
 import Text.Parsec
-       (Parsec, (<?>), (<|>), alphaNum, between, char, choice, eof, many,
-        many1, manyTill, noneOf, oneOf, option, optional, parse, sepBy,
-        skipMany, spaces, string, try)
+       (Parsec, ParseError, SourcePos, (<?>), (<|>), alphaNum, between,
+        char, choice, eof, incSourceLine, many, many1, manyTill, noneOf,
+        oneOf, option, optional, optionMaybe, parse, sepBy, skipMany,
+        spaces, string, tokenPrim, try)
 import Text.Shakespeare.Base
        (Ident(Ident), Deref, parseDeref, parseHash)
 
@@ -31,16 +33,16 @@ data Control
   | ControlElseIf Deref
   | ControlEndIf
   | NoControl Content
-  deriving (Show, Eq, Read, Data, Typeable)
+  deriving (Data, Eq, Read, Show, Typeable)
 
 data Doc = DocForall Deref Binding [Doc]
          | DocCond [(Deref, [Doc])] (Maybe [Doc])
          | DocContent Content
-    deriving (Show, Eq, Read, Data, Typeable)
+    deriving (Data, Eq, Read, Show, Typeable)
 
 data Content = ContentRaw String
              | ContentVar Deref
-    deriving (Show, Eq, Read, Data, Typeable)
+    deriving (Data, Eq, Read, Show, Typeable)
 
 type UserParser = Parsec String ()
 
@@ -53,7 +55,76 @@ docFromString s =
 parseDoc :: String -> Either String [Doc]
 parseDoc s = do
   controls <- parseLineControl s
-  return $ controlsToDocs controls
+  case parseDocFromControls controls of
+    Left parseError -> Left $ show parseError
+    Right docs -> Right docs
+
+type DocParser = Parsec [Control] ()
+
+parseDocFromControls :: [Control] -> Either ParseError [Doc]
+parseDocFromControls = parse docsParser ""
+
+docsParser :: DocParser [Doc]
+docsParser = many docParser
+
+docParser :: DocParser Doc
+docParser = forallDoc <|> condDoc <|> contentDoc
+
+contentDoc :: DocParser Doc
+contentDoc = tokenPrim show position noControlToDoc
+  where
+    position :: SourcePos -> Control -> [Control] -> SourcePos
+    position sourcePos _ _ = incSourceLine sourcePos 1
+
+    noControlToDoc :: Control -> Maybe Doc
+    noControlToDoc (NoControl content) = Just $ DocContent content
+    noControlToDoc _ = Nothing
+
+forallDoc :: DocParser Doc
+forallDoc = do
+  (ControlForall deref binding) <- forallControlStatement
+  innerDocs <- docsParser
+  void endforallControlStatement
+  pure $ DocForall deref binding innerDocs
+
+condDoc :: DocParser Doc
+condDoc = do
+  (ControlIf ifDeref) <- ifControlStatement
+  ifInnerDocs <- docsParser
+  maybeElseInnerDocs <- optionMaybe $ elseControlStatement *> docsParser
+  void endifControlStatement
+  pure $ DocCond [(ifDeref, ifInnerDocs)] maybeElseInnerDocs
+
+ifControlStatement :: DocParser Control
+ifControlStatement = primControlStatement $ \case
+  ControlIf deref -> Just $ ControlIf deref
+  _ -> Nothing
+
+elseControlStatement :: DocParser Control
+elseControlStatement = primControlStatement $ \case
+  ControlElse -> Just ControlElse
+  _ -> Nothing
+
+endifControlStatement :: DocParser Control
+endifControlStatement = primControlStatement $ \case
+  ControlEndIf -> Just ControlEndIf
+  _ -> Nothing
+
+primControlStatement :: (Control -> Maybe Control)-> DocParser Control
+primControlStatement = tokenPrim show incSourcePos
+
+incSourcePos :: SourcePos -> a -> b -> SourcePos
+incSourcePos sourcePos _ _ = incSourceLine sourcePos 1
+
+forallControlStatement :: DocParser Control
+forallControlStatement = primControlStatement $ \case
+  ControlForall deref binding -> Just $ ControlForall deref binding
+  _ -> Nothing
+
+endforallControlStatement :: DocParser Control
+endforallControlStatement = primControlStatement $ \case
+  ControlEndForall -> Just ControlEndForall
+  _ -> Nothing
 
 controlsToDocs :: [Control] -> [Doc]
 controlsToDocs [] = []
